724c0d98bf7e7d52ea02355679d652138c5d9238
friggito <ornella.friggit@gmail.com>
2022-07-08 18:05:38 +0200

    fix(rollup): compile from ts

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,12 @@
+import typescript from '@rollup/plugin-typescript';
+
 export default {
-    input: 'lib/index.js',
+    input: 'src/index.ts',
     output: {
       file: 'lib/bundle.js',
-      format: 'iife'
-    }
+      format: 'iife',
+      compact: true
+    },
+    plugins: [typescript()]
   };
   
